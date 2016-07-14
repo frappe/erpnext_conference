e16049ffb1733b9943af0c9d4201da7ab4decf6c
Neil Trini Lasrado <nlasrado@gmail.com>
2016-07-14 15:25:06 +0530

    Added logic to calculate total cost for participant tickets

--- erpnext_conference/erpnext_conference/doctype/participant/participant.js
+++ erpnext_conference/erpnext_conference/doctype/participant/participant.js
@@ -1,8 +1,4 @@
 // Copyright (c) 2016, Frappe Technologies and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Participant', {
-	refresh: function(frm) {
-
-	}
-});
+cur_frm.add_fetch("ticket","cost", "cost");
--- erpnext_conference/erpnext_conference/doctype/participant/participant.py
+++ erpnext_conference/erpnext_conference/doctype/participant/participant.py
@@ -3,8 +3,10 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
 from frappe.model.document import Document
 
 class Participant(Document):
-	pass
+	def validate(self):
+		self.total_cost = 0
+		for ticket in self.tickets:
+			self.total_cost += ticket.cost * ticket.quantity
